317671f575237ae7498958baaf216bc5a6ffb8b3
igor-86 <i.campoli86@gmail.com>
2022-11-14 11:39:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
