0363aa316921178793953066211937609cc762d3
igor-86 <i.campoli86@gmail.com>
2022-11-14 17:41:59 +0100

    Milestone 3 poster image

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -5,7 +5,8 @@ export default {
     data() {
         return {
             store,
-            name: "Results",
+            nameF: "Movies",
+            nameS: "Series",
             imgLang: "de.svg",
         }
     },
@@ -18,18 +19,24 @@ export default {
 </script>
 
 <template>
-    <h2>{{ name }}</h2>
+    <h2>{{ nameF }}</h2>
     <div class="list" v-for="(inner, index) in this.store.listFilm" :key="index">
+        <div class="img-poster">
+            <img :src="`http://image.tmdb.org/t/p/w342/${inner.poster_path}`" alt="">
+        </div>
         <h4>{{ inner.title }}</h4>
         <h4>{{ inner.original_title }}</h4>
         <img :src="getImagePath(`../assets/img/${inner.original_language}.svg`)" alt="">
         <p>{{ inner.vote_average }}</p>
     </div>
-    <h2>{{ name }}</h2>
+    <h2>{{ nameS }}</h2>
     <div class="list-serie" v-for="(series, index) in this.store.listSeries" :key="index">
+        <div class="img-poster">
+            <img :src="`http://image.tmdb.org/t/p/w342/${series.poster_path}`" alt="">
+        </div>
         <h4>{{ series.name }}</h4>
         <h4>{{ series.original_name }}</h4>
-        <p>{{ series.original_language }}</p>
+        <img :src="getImagePath(`../assets/img/${series.original_language}.svg`)" alt="">
         <p>{{ series.vote_average }}</p>
     </div>
 
@@ -37,6 +44,17 @@ export default {
 
 <style lang="scss" scoped>
 @use "../styles/general.scss" as *;
+
+.img-poster {
+    width: 200px;
+    height: 250px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+}
 
 img {
     width: 30px;
